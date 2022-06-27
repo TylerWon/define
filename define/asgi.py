539ee93d4a6458ebeb42e2e9f916525bb219a2f7
TylerWon <tywon@shaw.ca>
2022-06-26 21:48:46 -0700

    Initial commit

---
+++ define/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for define project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'define.settings')
+
+application = get_asgi_application()
