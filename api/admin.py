d0bbea52bd9e388da721ae61240495538ab51283
TylerWon <tywon@shaw.ca>
2022-06-27 23:03:45 -0700

    Create admin classes for Word, Type, and Definition models

--- api/admin.py
+++ api/admin.py
@@ -6,6 +6,18 @@ from .models import (
   Type,
 )
 
-admin.site.register(Word)
-admin.site.register(Type)
-admin.site.register(Definition)
+class WordAdmin(admin.ModelAdmin):
+  list_display = ["id", "spelling"]
+  list_filter = ["id", "spelling"]
+
+class TypeAdmin(admin.ModelAdmin):
+  list_display = ["id", "name"]
+  list_filter = ["id", "name"]
+
+class DefinitionAdmin(admin.ModelAdmin):
+  list_display = ["id", "word", "type"]
+  list_filter = ["id", "word", "type"]
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Definition, DefinitionAdmin)
