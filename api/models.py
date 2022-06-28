11d4a4a4dbc7b79d60984bdbbf508286622ed689
TylerWon <tywon@shaw.ca>
2022-06-27 23:05:44 -0700

    Make spelling field for Word model and name field for Type model unique

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Model that represents a word
 class Word(models.Model):
   users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="words")
-  spelling = models.CharField(max_length=50)
+  spelling = models.CharField(unique=True, max_length=50)
   pronunciation = models.CharField(max_length=75)
 
   def __str__(self):
@@ -12,7 +12,7 @@ class Word(models.Model):
 
 # Model that represents a type of word (i.e. noun, verb. adjective, etc.)
 class Type(models.Model):
-  name = models.CharField(max_length=25)
+  name = models.CharField(unique=True, max_length=25)
 
   def __str__(self):
     return self.name
