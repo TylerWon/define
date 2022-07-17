403ee0450c55364da0f5ea4474068ca91196e2d3
TylerWon <tywon@shaw.ca>
2022-07-16 23:27:39 -0700

    Create login thunk for userSlice

--- frontend/src/state/slices/userSlice.js
+++ frontend/src/state/slices/userSlice.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -9,8 +10,44 @@ const initialState = {
   error: null,
 };
 
+export const login = createAsyncThunk("user/login", async (email, password) => {
+  const data = {
+    email: email,
+    password: password
+  }
+
+  const response = await axios.post("/api/login", data);
+  return response;
+})
+
 export const userSlice = createSlice({
   name: "user",
   initialState: initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(login.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        const response = action.payload;
+
+        if (response === 200) {
+          const user = response.data;
+
+          state.status = "fulfilled";
+          state.id = user.id;
+          state.email = user.email;
+          state.firstName = user.firstName;
+          state.lastName = user.lastName;
+        } else {
+          state.status = "failed";
+          state.error = response.data.message;
+        }
+      })
+      .addCase(login,rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+  }
 })
