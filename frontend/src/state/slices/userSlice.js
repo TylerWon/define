78e51fabafeb95e4f6a780018826c9119d56be2f
TylerWon <tywon@shaw.ca>
2022-07-16 23:34:10 -0700

    Create logout thunk for userSlice

--- frontend/src/state/slices/userSlice.js
+++ frontend/src/state/slices/userSlice.js
@@ -10,6 +10,7 @@ const initialState = {
   error: null,
 };
 
+// Thunk for logging a User in
 export const login = createAsyncThunk("user/login", async (email, password) => {
   const data = {
     email: email,
@@ -18,7 +19,13 @@ export const login = createAsyncThunk("user/login", async (email, password) => {
 
   const response = await axios.post("/api/login", data);
   return response;
-})
+});
+
+// Thunk for logging a User out
+export const logout = createAsyncThunk("user/logout", async () => {
+  const response = await axios.get("/api/logout/");
+  return response;
+});
 
 export const userSlice = createSlice({
   name: "user",
@@ -46,6 +53,17 @@ export const userSlice = createSlice({
         }
       })
       .addCase(login,rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      
+      .addCase(logout.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state = initialState;
+      })
+      .addCase(logout,rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
       })
