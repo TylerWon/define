a164446a1c7d7732721e0e544bfc4fd9c9443b1c
TylerWon <tywon@shaw.ca>
2022-07-17 17:57:16 -0700

    Add axios interceptor that adds a csrf token to every request before they are sent

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,23 @@
+import axios from "axios";
+import Cookies from "js-cookie";
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import store from "./state/store";
+
+// Add request interceptor
+axios.interceptors.request.use((config) => {
+  // Add csrftoken (if it exists) to the request before it is sent
+  const csrftoken = Cookies.get("csrftoken");
+
+  if (csrftoken) {
+    config.headers["X-CSRFToken"] = csrftoken;
+  }
+  
+  return config;
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
