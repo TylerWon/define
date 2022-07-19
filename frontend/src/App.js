ff843f33462817b67bc9e5bfc65ce12c1db27683
TylerWon <tywon@shaw.ca>
2022-07-18 22:52:13 -0700

    Define index path using regex

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,31 @@ import {
   Route,
   Routes, 
 } from "react-router-dom";
+import { useDispatch } from 'react-redux'
+import { getWords } from "./state/slices/wordsSlice";
+import { useEffect } from "react";
 
 export default function App(props) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWords(15));
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<HeaderAndFooter />}> */}
-          {/* <Route index element={<Home />} /> */}
-          {/* <Route path="login" element={<Login />} /> */}
-          {/* <Route path="mywords" element={<MyWords />} /> */}
-          {/* <Route path="register" element={<Register />} /> */}
-          {/* <Route path="search" element={<Search />} /> */}
-        {/* </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <h1>HI</h1>
   )
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       {/* <Route path="/" element={<HeaderAndFooter />}> */}
+  //         {/* <Route index element={<Home />} /> */}
+  //         {/* <Route path="login" element={<Login />} /> */}
+  //         {/* <Route path="mywords" element={<MyWords />} /> */}
+  //         {/* <Route path="register" element={<Register />} /> */}
+  //         {/* <Route path="search" element={<Search />} /> */}
+  //       {/* </Route> */}
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index/index.html')),
+    re_path(r'.*', TemplateView.as_view(template_name='index/index.html')), # Put other paths before this one otherwise this path will take precedence
 ]
