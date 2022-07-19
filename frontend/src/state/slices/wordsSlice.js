9af62c18e713b4cc62ffb9e6cbab0905cfdc3601
TylerWon <tywon@shaw.ca>
2022-07-18 22:39:48 -0700

    Create getWords thunk for wordsSlice

--- frontend/src/state/slices/wordsSlice.js
+++ frontend/src/state/slices/wordsSlice.js
@@ -7,10 +7,39 @@ const initialState = {
   error: null
 };
 
+// Thunk for retrieving a User's words
+export const getWords = createAsyncThunk("words/get", async (userId) => {
+  const response = await axios.get(`/api/users/${userId}/words_and_definitions/`);
+  return response.data;
+})
+
 export const wordsSlice = createSlice({
   name: "words",
   initialState: initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getWords.fulfilled, (state, action) => {
+        const words = action.payload;
+
+        state.words = state.words.concat(words);
+        state.status = "fulfilled";
+      })
+
+      .addMatcher((action) => {
+        return action.type == getWords.pending;
+      }, (state, action) => {
+        state.status = "pending";
+      })
+
+      .addMatcher((action) => {
+        return action.type == getWords.rejected;
+      }, (state, action) => {
+        state.status = "rejected";
+        state.error = action.error.message;
+      })
+  }
+
 });
 
 export default wordsSlice.reducer;
