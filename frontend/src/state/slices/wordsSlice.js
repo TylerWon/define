93577abf06a3cf35e990322c939c70d585914017
TylerWon <tywon@shaw.ca>
2022-07-18 23:25:19 -0700

    Create addWord thunk for wordsSlice

--- frontend/src/state/slices/wordsSlice.js
+++ frontend/src/state/slices/wordsSlice.js
@@ -11,6 +11,17 @@ const initialState = {
 export const getWords = createAsyncThunk("words/get", async (userId) => {
   const response = await axios.get(`/api/users/${userId}/words_and_definitions/`);
   return response.data;
+});
+
+// Thunk for adding a Word to a User's words
+export const addWord = createAsyncThunk("words/add", async (data) => {
+  try {
+    const response = await axios.get(`/api/words/${data.spelling}/word_and_definitions/`);
+    return response.data;
+  } catch(e) {
+    const response = await axios.post("/api/word_and_definitions/", data);
+    return response.data;
+  }
 })
 
 export const wordsSlice = createSlice({
@@ -22,18 +33,25 @@ export const wordsSlice = createSlice({
       .addCase(getWords.fulfilled, (state, action) => {
         const words = action.payload;
 
-        state.words = state.words.concat(words);
         state.status = "fulfilled";
+        state.words = state.words.concat(words);
+      })
+
+      .addCase(addWord.fulfilled, (state, action) => {
+        const word = action.payload;
+
+        state.status = "fulfilled";
+        state.words = state.words.concat(word);
       })
 
       .addMatcher((action) => {
-        return action.type == getWords.pending;
+        return action.type == getWords.pending || action.type == addWord.pending;
       }, (state, action) => {
         state.status = "pending";
       })
 
       .addMatcher((action) => {
-        return action.type == getWords.rejected;
+        return action.type == getWords.rejected || action.type == addWord.rejected;
       }, (state, action) => {
         state.status = "rejected";
         state.error = action.error.message;
