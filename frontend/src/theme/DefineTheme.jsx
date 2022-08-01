27b0eae5e3b84d854cd64afe5f73d9460bf26de1
TylerWon <tywon@shaw.ca>
2022-08-01 00:11:35 -0700

    Define h1-h6 typography variants, set default font families, set color palette

---
+++ frontend/src/theme/DefineTheme.jsx
@@ -0,0 +1,55 @@
+import { 
+  createTheme, 
+  responsiveFontSizes, 
+  ThemeProvider 
+} from "@mui/material";
+
+export const defineTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0056FE"
+    },
+    secondary: {
+      main: "#F1EEEA"
+    },
+    bgPrimary: {
+      main: "#FFFFFF"
+    },
+    bgSecondary: {
+      main: "#F6F3F0"
+    }
+  },
+  typography: {
+    fontFamily: "Inter, Roboto, Helvetica, Arial, sans-serif",
+    h1: {
+      fontWeight: "700",
+      fontSize: "48px"
+    },
+    h2: {
+      fontWeight: "700",
+      fontSize: "42px"
+    },
+    h3: {
+      fontWeight: "700",
+      fontSize: "36px"
+    },
+    h4: {
+      fontWeight: "700",
+      fontSize: "30px"
+    },
+    h5: {
+      fontWeight: "700",
+      fontSize: "24px"
+    },
+    h6: {
+      fontWeight: "700",
+      fontSize: "18px"
+    }
+  },
+})
+
+export default function DefineTheme(props) {
+  const theme = responsiveFontSizes(defineTheme);
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+}
