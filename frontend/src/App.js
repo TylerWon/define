490c47c580a86cf27a5b616258d9df2ec0b168a9
TylerWon <tywon@shaw.ca>
2022-08-02 21:50:02 -0700

    Use DefineTheme on App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,28 @@ import {
   Routes
 } from "react-router-dom";
 
+import DefineTheme from "./theme/DefineTheme";
+
 export default function App(props) {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Header />}>
-          <Route path="/" element={<Home />} />
+    <DefineTheme>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Header />}>
+            <Route path="/" element={<Home />} />
 
-          <Route element={<AuthorizedRoutes />}>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings/account" element={<AccountSettings />} />
+            <Route element={<AuthorizedRoutes />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/settings/account" element={<AccountSettings />} />
+            </Route>
           </Route>
-        </Route>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </DefineTheme>
   )
 }
