d1e1aa2bc555a578c80a1be766b0a95e148f1a28
TylerWon <tywon@shaw.ca>
2022-08-06 16:30:37 -0700

    Position Home page footer at the bottom of the page, use Grid instead of Stack to implement footer

--- frontend/src/components/Home/Footer.jsx
+++ frontend/src/components/Home/Footer.jsx
@@ -1,4 +1,5 @@
 import { 
+  Grid,
   Link,
   Stack, 
   Typography 
@@ -9,21 +10,31 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 // The footer for the Home page
 export default function Footer(props) {
   return (
-    <Stack
-      direction="row"
-      spacing={2}
+    <Grid
+      container
+      position="absolute"
+      bottom="0px"
       alignItems="center"
       justifyContent="flex-start"
       bgcolor="black"
-      sx={{ padding: "0.5%" }}
+      sx={{ 
+        width: "100%", 
+        padding: "0.5%"
+      }}
     >
-      <Typography color="white">Created by Tyler Won</Typography>
-      <Link href="https://github.com/TylerWon" target="_blank" rel="noopener">
-        <GitHubIcon fontSize="large" sx={{ color: "white" }}/>
-      </Link>
-      <Link href="https://www.linkedin.com/in/tyler-won-315907234/" target="_blank" rel="noopener">
-        <LinkedInIcon fontSize="large" sx={{ color: "white" }}/>
-      </Link>
-    </Stack>
+      <Grid item>
+        <Typography color="white">Created by Tyler Won</Typography>
+      </Grid>
+      <Grid item sx={{ marginLeft: "10px" }}>
+        <Link href="https://github.com/TylerWon" target="_blank" rel="noopener">
+          <GitHubIcon fontSize="large" sx={{ color: "white" }}/>
+        </Link>
+      </Grid>
+      <Grid item sx={{ marginLeft: "10px" }}>
+        <Link href="https://www.linkedin.com/in/tyler-won-315907234/" target="_blank" rel="noopener">
+          <LinkedInIcon fontSize="large" sx={{ color: "white" }}/>
+        </Link>
+      </Grid>
+    </Grid>
   )
 }
