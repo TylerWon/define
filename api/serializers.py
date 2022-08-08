85b749f374ade889fe573f9474d6ab2a9fafbb20
TylerWon <tywon@shaw.ca>
2022-08-07 19:02:07 -0700

    Remove serializers related to Definition and Type models

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import (
-  Word,
-  Definition,
-  Type,
-)
+from .models import Word
 
 # Serializer for the User model
 class UserSerializer(serializers.ModelSerializer):
@@ -57,56 +52,5 @@ class WordSerializer(serializers.ModelSerializer):
       "id",
       "users",
       "spelling",
-      "pronunciation",
+      "word_class"
     ]
-
-# Serializer for the Type model
-class TypeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Type
-    fields = [
-      "id",
-      "name",
-    ]
-
-# Serializer for the Definition model
-class DefinitionSerializer(serializers.ModelSerializer):
-
-  class Meta:
-    model = Definition
-    fields = [
-      "id",
-      "word",
-      "type",
-      "definition_text",
-    ]
-
-# Serializer for the Word model where the Definitions for the Word are nested
-class WordAndDefinitionsSerializer(serializers.ModelSerializer):
-  word_definitions = DefinitionSerializer(many=True)
-
-  class Meta:
-    model = Word
-    fields = [
-      "id",
-      "users",
-      "spelling",
-      "pronunciation",
-      "word_definitions",
-    ]
-  
-  # Override default create method to handle write operations with nested serializer
-  def create(self, validated_data):
-    # Pop data that is not for a Word
-    definitions_data = validated_data.pop("word_definitions")
-    users = validated_data.pop("users")
-
-    # Create a Word add users to the Word after because many-to-many relationship
-    word = Word.objects.create(**validated_data)
-    word.users.add(*users)   
-
-    # Create Definitions, associating each with the Word
-    for definition_data in definitions_data:
-      definition = Definition.objects.create(word=word, **definition_data)
-    
-    return word
