e6e59d42f23852d347ee09b6de0a567c3956f416
TylerWon <tywon@shaw.ca>
2022-08-07 18:58:32 -0700

    Remove Definition and Type models

--- api/models.py
+++ api/models.py
@@ -5,20 +5,4 @@ from django.db import models
 class Word(models.Model):
   users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="words")
   spelling = models.CharField(unique=True, max_length=50)
-  pronunciation = models.CharField(max_length=75)
-
-  def __str__(self):
-    return self.spelling
-
-# Model that represents a type of word (i.e. noun, verb. adjective, etc.)
-class Type(models.Model):
-  name = models.CharField(unique=True, max_length=25)
-
-  def __str__(self):
-    return self.name
-
-# Model that represents a word definition
-class Definition(models.Model):
-  word = models.ForeignKey(Word, on_delete=models.CASCADE, null=True, blank=True, related_name="word_definitions")
-  type = models.ForeignKey(Type, on_delete=models.CASCADE, related_name="type_definitions")
-  definition_text = models.CharField(max_length=350)
+  word_class = models.CharField(max_length=25)
