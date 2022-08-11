62d915b0d7cab710d83f118bc338478790e5e241
TylerWon <tywon@shaw.ca>
2022-08-10 22:53:23 -0700

    Remove wordsSlice

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+
 import userReducer from "./slices/userSlice";
-import wordsReducer from "./slices/wordsSlice";
 
 export default configureStore({
   reducer: {
-    user: userReducer,
-    words: wordsReducer
-  },
+    user: userReducer
+  }
 });
