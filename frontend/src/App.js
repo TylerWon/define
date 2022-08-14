6cd4adfa62c06612c4d5dc9cd1437ff6f95edb87
TylerWon <tywon@shaw.ca>
2022-08-13 18:49:55 -0700

    Remember user if previously logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { 
   BrowserRouter,
   Route,
@@ -11,28 +13,59 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
 import SearchResult from "./components/SearchResult";
+import { getUser } from "./state/slices/userSlice";
 
 export default function App(props) {
+  // State
+  const [loading, setLoading] = useState(true);
+
+  // React Redux hooks
+  const dispatch = useDispatch();
+
+  // Dispatches the getUser action
+  const dispatchGetUser = async (userId) => {
+    try {
+      await dispatch(getUser(userId)).unwrap();
+    } catch(e) {
+      // do nothing
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // Effect: On initial render, get user info if previously logged in
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    if (userId !== null) dispatchGetUser(userId);
+    else setLoading(false);
+  }, [])
 
   return (
-    <DefineTheme>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Navbar />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/search/:word" element={<SearchResult />} />
-
-            {/* <Route element={<AuthorizedRoutes />}> */}
-              {/* <Route path="/profile" element={<Profile />} /> */}
-              {/* <Route path="/settings/account" element={<AccountSettings />} /> */}
-            {/* </Route> */}
-          </Route>
-
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </DefineTheme>
+    <>
+      {!loading ? 
+        <DefineTheme>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<Navbar />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/search/:word" element={<SearchResult />} />
+  
+                {/* <Route element={<AuthorizedRoutes />}> */}
+                  {/* <Route path="/profile" element={<Profile />} /> */}
+                  {/* <Route path="/settings/account" element={<AccountSettings />} /> */}
+                {/* </Route> */}
+              </Route>
+  
+              {/* <Route path="*" element={<NotFound />} /> */}
+            </Routes>
+          </BrowserRouter>
+        </DefineTheme>
+      :
+        null
+      }
+    </>
   )
 }
