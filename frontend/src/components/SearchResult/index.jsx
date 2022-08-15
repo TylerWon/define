bb2a1fce1e0ca0ee31895e3c80f9db6415213a4c
TylerWon <tywon@shaw.ca>
2022-08-14 20:19:32 -0700

    Change background color of the Search Result page's body element

--- frontend/src/components/SearchResult/index.jsx
+++ frontend/src/components/SearchResult/index.jsx
@@ -3,13 +3,14 @@ import { Stack } from "@mui/material";
 import Definition from "./Definition";
 import SearchBar from "./SearchBar";
 
+import "./index.css";
+
 // The Search Result page
 export default function SearchResult(props) {
   return (
     <Stack 
       alignItems="center"
       justifyContent="flex-start"
-      bgcolor="bgSecondary.main"
     >
       <SearchBar />
       <Definition />
