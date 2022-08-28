1886f88f58ec045f4b9a93954a735570a7570c88
TylerWon <tywon@shaw.ca>
2022-08-28 00:09:20 -0700

    Remove LoginAndRegister component, refactor Login and Sign Up pages to accomodate removal of it

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,18 +1,49 @@
-import LoginAndRegister from "../lib/LoginAndRegister";
+import {
+  Grid,
+  Link as MuiLink,
+  Stack,
+  Typography
+} from "@mui/material";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 import LoginForm from "./LoginForm";
 
 // The Login page
 export default function Login(props) {
   return (
-    <LoginAndRegister
-      title="Login"
-      description="Access your Define account"
-      question="Don't have an account?"
-      answer="Sign up instead"
-      route="/register"
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        width: "100%",
+        paddingTop: "125px"
+      }}
     >
-      <LoginForm />
-    </LoginAndRegister>
+      <Grid item xs={10} sm={6} md={5} lg={4} xl={3}>
+        <Stack
+          spacing={4}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ width: "100%" }}
+        >
+          <Stack
+            spacing={1}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="h1">Login</Typography>
+            <Typography>Access your Define account</Typography>
+          </Stack>
+          <LoginForm />
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography>Don't have an account? <MuiLink component={ReactRouterLink} to="/register" underline="none">Sign up</MuiLink></Typography>
+          </Stack>
+        </Stack>
+      </Grid>
+    </Grid>
   )
 }
--- frontend/src/components/Register/index.jsx
+++ frontend/src/components/Register/index.jsx
@@ -1,18 +1,44 @@
-import LoginAndRegister from "../lib/LoginAndRegister";
+import {
+  Grid,
+  Link as MuiLink,
+  Stack,
+  Typography
+} from "@mui/material";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 import SignUpForm from "./SignUpForm";
 
 // The Sign Up page
 export default function Register(props) {
   return (
-    <LoginAndRegister
-      title="Sign up"
-      description="Create your Define account"
-      question="Already have an account?"
-      answer="Sign in instead"
-      route="/login"
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        width: "100%",
+        paddingTop: "125px"
+      }}
     >
-      <SignUpForm />
-    </LoginAndRegister>
+      <Grid item xs={10} sm={6} md={5} lg={4} xl={3}>
+        <Stack
+          spacing={4}
+          alignItems="center"
+          justifyContent="center"
+          sx={{ width: "100%" }}
+        >
+          <Stack
+            spacing={1}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="h1">Sign up</Typography>
+            <Typography>Create your Define account</Typography>
+          </Stack>
+          <SignUpForm />
+          <Typography>Already have an account? <MuiLink component={ReactRouterLink} to="/login" underline="none">Sign in</MuiLink></Typography>
+        </Stack>
+      </Grid>
+    </Grid>
   )
 }
